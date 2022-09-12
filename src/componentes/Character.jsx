562ca6338288dc06016f6ccec8bec792cbbaccee
Max dc <mcangreq@outlook.com>
2022-09-11 22:45:48 -0500

    first commit

---
+++ src/componentes/Character.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const Character = () => {
+
+    const [person, setPerson] = useState({});
+    const [location, setLocation] = useState({})
+    const combineId = Math.floor(Math.random() * 20) + 1
+    const [placeId, setPlaceId] = useState('')
+
+    const searchId = () => {
+        axios.get(`https://rickandmortyapi.com/api/location/${placeId}`)
+            .then(res => setLocation(res.data))
+        axios.get(`https://rickandmortyapi.com/api/character/${placeId}`)
+            .then(res => setPerson(res.data))
+    }
+    useEffect(() => {
+
+        axios.get(`https://rickandmortyapi.com/api/character/${combineId}`)
+            .then(res => setPerson(res.data))
+    }, [])
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/location/${combineId}`)
+            .then(res => setLocation(res.data))
+    }, [])
+
+
+    // console.log(location.residents.length)
+    return (
+        <div className='box_origin'>
+            <div className='container_character'>
+                <div className='boxCharacter'>
+                    <h1 className='title'>{person.name}</h1>
+                    <div className='box_img'>
+                        <img className='img' src={person.image} alt="" />
+
+
+                    </div>
+                </div>
+            </div>
+            <div className='container_ubication' >
+                <h2 className='ubication_title'>
+                    {location.name}
+                </h2>
+                <div className='box_parrafos'>
+                    <p className='letra'>Type : {location.type}</p>
+                    <p className='letra'>Dimension : {location.dimension}</p>
+                    <p className='letra'>Residents : {location.residents.length}</p>
+                   
+                </div>
+                <div className='box_input'>
+                    <label onClick={searchId}><i class="fa-solid fa-magnifying-glass"></i>search</label>
+                    <input type="text" value={placeId} onChange={e => setPlaceId(e.target.value)} />
+                </div>
+                
+            </div>
+        </div>
+
+    );
+};
+
+export default Character;
